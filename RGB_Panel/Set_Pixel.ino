059fff42cf5b16c76853fb377cb1db51fac12016
Aaron Corrales <acorrales@capsonic.com>
2021-03-13 18:59:21 -0700

    First Commit

---
+++ RGB_Panel/Set_Pixel.ino
@@ -0,0 +1,51 @@
+void Sparkles()
+{
+
+int R_Pixel_C=random(0, Columns);
+int R_Pixel_R=random(0, Rows);
+byte R_RGB_R=random(0, 255);
+byte R_RGB_G=random(0, 255);
+byte R_RGB_B=random(0, 255);
+Set_Pixel(R_Pixel_C, R_Pixel_R, R_RGB_R, R_RGB_G, R_RGB_B);
+  
+  }
+void Set_Pixel(int Column,int Row,byte R,byte G,byte B)
+{int Pixel_No=(Column);
+  for(int i=0;i<Row;i++)
+  {
+  Pixel_No+=Columns;
+  }
+tira.setPixelColor(Pixel_No,R,G,B);
+tira.show();
+  delay(retardo);
+  }
+  void Clear_Panel()
+  {
+        
+for (int i=0;i<nleds;i++){ 
+ tira.setPixelColor(i,0,0,0); // Apagado
+ //tira.show();
+ //delay(retardo);
+ //tira.setPixelColor(i,0,0,0); 
+
+ } 
+ tira.show();
+ delay(500);
+  Serial.println("Panel Cleared!");
+    }
+    
+  void Pixel_Check()
+  {
+        
+  Serial.print("Qty of Pixels: ");
+  Serial.println(nleds);
+for (int i=0;i<nleds;i++){ 
+ tira.setPixelColor(i,255,255,255); //Encendido
+ tira.show();
+ delay(retardo);
+ tira.setPixelColor(i,0,0,0); // Apagado
+ tira.show();
+
+ } 
+ delay(500);
+    }

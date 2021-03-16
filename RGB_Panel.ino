02b766c9bcdfe78609ecf314343089e5d110764f
Aaron Corrales <acorrales@capsonic.com>
2021-03-16 00:02:24 -0600

    Visual Studio support

--- RGB_Panel.ino
+++ RGB_Panel.ino
@@ -20,69 +20,83 @@ bool stringComplete = false;  // whether the string is complete
 
 Adafruit_NeoPixel tira = Adafruit_NeoPixel(nleds, 3, NEO_GRB + NEO_KHZ800);  // Pin 3 del Arduino es salida de datos
 
-void setup(){ 
-  nleds=(Columns)*(Rows);
-  tira.begin();       // inicializacion de la tira
-  tira.show(); 
-  Serial.begin(9600);
-  while (!Serial) {
-    
-    ; // wait for serial port to connect. Needed for native USB port only
-  }    
-  Clear_Panel();
-  Serial.println("Ready!!"); 
-  //Pixel_Check();
-  inputString.reserve(200); 
-tira.setBrightness(brillo);
+void setup() {
+    nleds = (Columns) * (Rows);
+    tira.begin();       // inicializacion de la tira
+    tira.show();
+    Serial.begin(9600);
+    while (!Serial) 
+    {
+    }
+    Clear_Panel();
+    Serial.println("Ready!!");
+    //Pixel_Check();
+    inputString.reserve(200);
+    tira.setBrightness(brillo);
 }
 
-void loop(){
-  ////////////////////////////////////////////////////
+void loop()
+{
+    ////////////////////////////////////////////////////
 
-   // print the string when a newline arrives:
-  if (stringComplete) {
-    Serial.println(inputString);
-    // clear the string:
-    inputString = "";
-    stringComplete = false;
-  }
-Sparkles();
-Set_Pixel(0,0,255,0,0); //Red
-Set_Pixel(7,0,255,0,0); //Red
-Set_Pixel(0,7,255,0,0); //Red
-Set_Pixel(7,7,255,0,0); //Red
+     // print the string when a newline arrives:
+    if (stringComplete)
+    {
+        Serial.println(inputString);
+        // clear the string:
+        inputString = "";
+        stringComplete = false;
+    }
+    //Sparkles();
+    Set_Pixel(0, 0, 255, 0, 0); //Red
+    Set_Pixel(7, 0, 255, 0, 0); //Red
+    Set_Pixel(0, 7, 255, 0, 0); //Red
+    Set_Pixel(7, 7, 255, 0, 0); //Red
 
-Set_Pixel(8,0,0,255,0); //Green
-Set_Pixel(15,0,0,255,0); //Green
-Set_Pixel(8,7,0,255,0); //Green
-Set_Pixel(15,7,0,255,0); //Green
+    Set_Pixel(8, 0, 0, 255, 0); //Green
+    Set_Pixel(15, 0, 0, 255, 0); //Green
+    Set_Pixel(8, 7, 0, 255, 0); //Green
+    Set_Pixel(15, 7, 0, 255, 0); //Green
 
-Set_Pixel(0,8,0,0,255); //Blue
-Set_Pixel(7,8,0,0,255); //Blue
-Set_Pixel(0,15,0,0,255); //Blue
-Set_Pixel(7,15,0,0,255); //Blue
+    Set_Pixel(0, 8, 0, 0, 255); //Blue
+    Set_Pixel(7, 8, 0, 0, 255); //Blue
+    Set_Pixel(0, 15, 0, 0, 255); //Blue
+    Set_Pixel(7, 15, 0, 0, 255); //Blue
 
-Set_Pixel(15,8,255,255,0); //Yellow
-Set_Pixel(8,15,255,255,0); //Yellow
-Set_Pixel(15,15,255,255,0); //Yellow
-Set_Pixel(8,8,255,255,0); //Yellow
-////////////////////////////////////////////////////////
+    Set_Pixel(15, 8, 255, 255, 0); //Yellow
+    Set_Pixel(8, 15, 255, 255, 0); //Yellow
+    Set_Pixel(15, 15, 255, 255, 0); //Yellow
+    Set_Pixel(8, 8, 255, 255, 0); //Yellow
+    ////////////////////////////////////////////////////////
 }
 /*
   SerialEvent occurs whenever a new data comes in the hardware serial RX. This
   routine is run between each time loop() runs, so using delay inside loop can
   delay response. Multiple bytes of data may be available.
 */
-void serialEvent() {
-  while (Serial.available()) {
-    // get the new byte:
-    char inChar = (char)Serial.read();
-    // add it to the inputString:
-    inputString += inChar;
-    // if the incoming character is a newline, set a flag so the main loop can
-    // do something about it:
-    if (inChar == '\n') {
-      stringComplete = true;
+void serialEvent()
+{
+    //int counter = 0;
+    while (Serial.available())
+    {
+        // get the new byte:
+        char inChar = (char)Serial.read();
+        // add it to the inputString:
+        inputString += inChar;
+        //Serial.println(inputString);
+        // if the incoming character is a newline, set a flag so the main loop can
+        // do something about it:
+        if (inChar == '\n')
+        {
+            stringComplete = true;
+            //Serial.println("Finish Serial Event!!");
+        }
+        //else
+        //{
+        //    Serial.println(inChar);
+        //    counter++;
+        //}
     }
-  }
+    stringComplete = true;
+    //Serial.println(counter);
 }
